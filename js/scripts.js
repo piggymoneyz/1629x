1e0ac9de973efc5fc25b08cf59776e143a53b3ab
PiggyMoney <albert.haopeng.chen@gmail.com>
2025-02-26 22:58:31 -0800

    inital web pag

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function showContent(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+    document.getElementById(sectionId).style.display = 'block';
+}
